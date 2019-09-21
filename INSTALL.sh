97314759697cb0764a9a65a9b7fc1b3400691be5
jajalobrit <52551379+jajalobrit@users.noreply.github.com>
2019-09-21 11:44:56 -0300

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -14,7 +14,7 @@ printf "\n\n"
 echo "INSTALANDO IMAGEMAGICK!"
 sudo apt-get install imagemagick fbi
 printf "\n\n"
-echo "IMAGEMAGICK OK!"
+echo -e "\033[1;32m IMAGEMAGICK OK! \033[0m"
 
 ##### DOWNLOAD DOS ARQUIVOS NECESSÁRIOS #####
 printf "\n\n"
@@ -28,7 +28,7 @@ sudo chmod +x /usr/local/bin/pngview;
 cd /usr/local/bin/;
 sudo wget -O NONET.png "https://raw.githubusercontent.com/jajalobrit/RETROPIE-DATETIME/master/NONET.png";
 printf "\n\n"
-echo "ARQUIVOS OK!"
+echo -e "\033[1;32m ARQUIVOS OK! \033[0m"
 
 ##### CONFIGURA RUNCOMMANDS ####
 if
@@ -36,25 +36,25 @@ if
    echo -e "sudo pkill TIME.sh;
 sudo pkill -u root pngview" >> /opt/retropie/configs/all/runcommand-onstart.sh
 printf "\n\n"
-echo "RUNCOMMAND-ONSTART OK!"
+echo -e "\033[1;32m RUNCOMMAND-ONSTART OK! \033[0m"
 else
 cd /opt/retropie/configs/all/;
 sudo wget -O runcommand-onstart.sh "https://raw.githubusercontent.com/jajalobrit/RETROPIE-DATETIME/master/runcommand-onstart.sh";
 sudo chmod +x /opt/retropie/configs/all/runcommand-onstart.sh;
 printf "\n\n"
-echo "RUNCOMMAND-ONSTART OK!"
+echo -e "\033[1;32m RUNCOMMAND-ONSTART OK! \033[0m"
 fi
 if
 [ -d "/opt/retropie/configs/all/runcommand-onend.sh" ]; then
    echo -e "/usr/local/bin/TIME.sh &" >> /opt/retropie/configs/all/runcommand-onend.sh
 printf "\n\n"
-echo "RUNCOMMAND-ONEND OK!"
+echo -e "\033[1;32m RUNCOMMAND-ONNEND OK! \033[0m"
 else
 cd /opt/retropie/configs/all/;
 sudo wget -O runcommand-onend.sh "https://raw.githubusercontent.com/jajalobrit/RETROPIE-DATETIME/master/runcommand-onend.sh";
 sudo chmod +x /opt/retropie/configs/all/runcommand-onend.sh;
 printf "\n\n"
-echo "RUNCOMMAND-ONEND OK!"
+echo -e "\033[1;32m RUNCOMMAND-ONNEND OK! \033[0m"
 fi
 
 ##### INSTALA FONTE ######
@@ -62,7 +62,7 @@ sudo mkdir -p /usr/share/fonts/opentype &&
 cd /usr/share/fonts/opentype/
 sudo wget -O ORBITRON-BLACK.otf "https://raw.githubusercontent.com/jajalobrit/RETROPIE-DATETIME/master/ORBITRON-BLACK.otf";
 printf "\n\n"
-echo "FONTE OK!"
+echo -e "\033[1;32m FONTE OK! \033[0m"
 
 ##### CONFIGURA KODI.SH #####
 if
@@ -71,7 +71,7 @@ cd /home/pi/RetroPie/roms/kodi/
 sudo wget -O kodi.sh "https://raw.githubusercontent.com/jajalobrit/RETROPIE-DATETIME/master/kodi.sh";
 sudo chmod +x kodi.sh
 printf "\n\n"
-echo "KODI.SH OK!"
+echo -e "\033[1;32m KODI.SH OK! \033[0m"
 fi
 if
 [ -d "/home/pi/RetroPie/roms/ports" ]; then
@@ -79,7 +79,7 @@ cd /home/pi/RetroPie/roms/ports/
 sudo wget -O kodi.sh "https://raw.githubusercontent.com/jajalobrit/RETROPIE-DATETIME/master/kodi.sh";
 sudo chmod +x kodi.sh
 printf "\n\n"
-echo "KODI.SH OK!"
+echo -e "\033[1;32m KODI.SH OK! \033[0m"
 fi
 
 
@@ -88,18 +88,16 @@ fi
 if fgrep -q -e "/usr/local/bin/TIME.sh" /etc/rc.local
 then
 printf "\n\n"
-echo "RC.LOCAL OK!"
+echo -e "\033[1;32m RC.LOCAL OK! \033[0m"
 else
 	find /etc/ -iname rc.local -exec sudo sed -i '$ s,exit 0,/usr/local/bin/TIME.sh \&\
 exit 0,g' {} \;
 printf "\n\n"
-echo "RC.LOCAL OK!"
-
+echo -e "\033[1;32m RC.LOCAL OK! \033[0m"
 
 #####FINALIZADO"
 printf "\n\n\n"
-echo "INSTALACAO CONCLUIDA"
-echo "REINICIANDO EM 3 SEGUNDOS"
-echo "POR FAVOR AGUARDE"
+echo -e "\033[1;32m INSTALAÇÃO CONCLUÍDA \033[0m"
+echo -e "\033[1;32m REINICIANDO EM 3 SEGUNDOS \033[0m"
 sleep 3 
 sudo reboot
