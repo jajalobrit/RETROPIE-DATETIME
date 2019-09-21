28c54630645fbd1bddb1aa3a1a661645ce3937ff
RETROWAVE-ARCADE <52551379+jajalobrit@users.noreply.github.com>
2019-09-21 14:35:31 -0300

    Update TIME.sh

--- TIME.sh
+++ TIME.sh
@@ -15,11 +15,11 @@ case $? in
 				
 		## ajusta o horário da raspberry conforme a internet		
 	        sudo date -s "$(wget -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f5-8)Z" >/dev/null&&
-		## pega data e hora atuais e transforma em imagem png com fundo preto usando o imagemagik
+		## pega data e hora atuais e transforma em imagem png com fundo preto usando o imagemagick
 			sudo convert -size 360x87 xc:black -fill white -font Orbitron-Black -gravity east \
 			-pointsize 17 -draw "text 0,0 '$(date "+%A, %d de %B")'" \
 			-pointsize 30 -draw "text 0,32 '$(date "+%H:%M")'" /dev/shm/DATAHORA.png
-		## converte a imagem com fundo preto em imagem com fundo transparente usando o imagemagik
+		## converte a imagem com fundo preto em imagem com fundo transparente usando o imagemagick
 			sudo convert /dev/shm/DATAHORA.png -fuzz 50% -transparent black /dev/shm/DATAHORA.png;
 		## Exibe a imagem na tela durante 1 minuto usando o pngview, apaga e repete o processo	
 			sudo /usr/local/bin/pngview -b 0 -l 999997 -y 40 -x 1531 /dev/shm/DATAHORA.png & sleep 1m;sudo pkill -u root pngview;
@@ -37,9 +37,9 @@ case $? in
 esac
 done
 
- ##################################
-##          IMPORTANTE            ##
- ##################################
+                                                    ##################################
+                                                   ###          IMPORTANTE            ##
+                                                    ##################################
  
 ## O SCRIPT É INTERROMPIDO QUANDO EMULADOR/KODI É ABERTO E RETOMADO ATRAVES DOS RUNCOMMANDS E DO KODI.SH DISPONIBILIZADOS
 ## A REGIÃO PRECISA ESTAR CONFIGURADA NA RASPBERRY PARA QUE SEJAM EXIBIDAS DATA E HORA CORRETAS
