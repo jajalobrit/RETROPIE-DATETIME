7f8c481f906178fe0919f66188480f58aae08d17
jajalobrit <52551379+jajalobrit@users.noreply.github.com>
2019-09-21 11:56:04 -0300

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -75,6 +75,7 @@ sudo chmod +x kodi.sh
 printf "\n\n"
 echo -e "\033[1;32m KODI.SH OK! \033[0m"
 fi
+
 if
 [ -d "/home/pi/RetroPie/roms/ports" ]; then
 cd /home/pi/RetroPie/roms/ports/
@@ -83,7 +84,6 @@ sudo chmod +x kodi.sh
 printf "\n\n"
 echo -e "\033[1;32m KODI.SH OK! \033[0m"
 fi
-
 
 ##### COLOCA O SCRIPT PARA SER EXECUTADO AUTOMATICAMENTE DURANTE O BOOT #####
 if fgrep -q -e "/usr/local/bin/TIME.sh" /etc/rc.local
